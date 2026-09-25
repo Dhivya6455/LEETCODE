d87f8fbb962efdcc34a4ead33e0ba2ebe078f12f
Dhivya.S <sakthiveldhivya4@gmail.com>
2026-09-25 21:53:34 +0530

    Time: 1 ms (32.71%) | Memory: 48.6 MB (37.74%) - LeetSync

---
+++ 598-range-addition-ii/range-addition-ii.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public int maxCount(int m, int n, int[][] ops) {
+        if(ops.length==0){
+            return n*m;
+        }
+        int minRow = ops[0][0];
+
+        int minCol = ops[0][1];
+
+for(int i = 0; i < ops.length; i++) {
+    minRow = Math.min(minRow, ops[i][0]);
+    minCol = Math.min(minCol, ops[i][1]);
+}
+
+return minRow * minCol;
+    }
+}
